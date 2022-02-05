be455c7866500f6be8218df40b5b6229f3803686
Chirinosjor <chirinosjor@gmail.com>
2022-02-05 00:21:01 -0300

    Component connected to API successfully

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,37 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 
-const API = 'https://amiiboapi.com/api/amiibo/?amiiboSeries=0x06'
-const Products = () => {
-  const [products, setProducts] = useState([]);
-useEffect(() => {
-  fetchProducts();
-}, []);
-const fetchProducts = () => {
-  axios
-    .get('https://shoppingapiacme.herokuapp.com/shopping')
-    .then((res) => {
-      console.log(res);
-      setProducts(res.data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+const ProductList = () => {
+
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    getProducts()
+  }, [])
+
+  const getProducts = async () => {
+    const API = 'https://shoppingapiacme.herokuapp.com/shopping'
+    const data = await fetch(API)
+    const productList = await data.json()
+    setProducts(productList)
+  }
+
+  return <div>
+    <ul>
+      {
+        products.map(item => (
+          <li key={item.id} >{item.brand}</li>
+        ))
+      }
+    </ul>
+  </div>;
 };
-return (
-    <div>
-      <h1>Featured Products</h1>
-      <div className='item-container'>
-        {products.map((product) => (
-          <div className='card'>
-            <p>{product.brand}</p>
-            <img src={product.image} alt='' />
-            <h3>{product.brand}</h3>
-            <p>{product.item}</p>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-};
-export default Products;
+
+export default ProductList;
